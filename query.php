724621e1493bf8dd35db6358f3322f9b30ece1ab
Sumit Teli <business.ieltsforge@gmail.com>
2024-10-22 10:56:23 +0530

    Create query.php

---
+++ query.php
@@ -0,0 +1,32 @@
+<?php
+include 'database.php';
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['role_id'] = $user['role_id'];
+
+            if ($user['role_id'] == 1) {
+                header('Location: home_admin.php'); // Redirect to admin home
+            } else {
+                header('Location: home_user.php');  // Redirect to user home
+            }
+            exit();
+        } else {
+            echo "Invalid login!";
+        }
+    } else {
+        echo "Invalid login!";
+    }
+}
+?>
