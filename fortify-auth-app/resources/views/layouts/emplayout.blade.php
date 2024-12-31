b13c7b101a84c74b3b32362badc186d06fb30b4c
shrutimoontechnolabs <shruti@moontechnolabs.com>
2024-12-31 13:42:08 +0530

    noti

--- fortify-auth-app/resources/views/layouts/emplayout.blade.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit" class="flex items-center">
-        <i class="ti-layout-sidebar-left"></i> Logout
-    </button>
-</form>
